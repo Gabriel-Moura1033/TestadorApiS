0542f0a4c30be191e51fbb545b8b37490ecace05
Gabriel Moura <rez2000@outlook.com>
2023-10-09 22:38:35 -0300

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,11 +4,11 @@ dotenv.config()
 export const config = {
     user: process.env.USERDB,
     password: process.env.SENHADB,
-    server: 'localhost', // Substitua pelo endereço do servidor SQL Server
+    server: 'localhost', 
    // port: 1443,
     database: 'Testa_Api',
     options: {
-      encrypt: true, // Use esta opção se você estiver usando uma conexão segura (SSL/TLS)
+      encrypt: true,
       trustServerCertificate: true, // essa opção serve pra confiar em um certificado autoemitido :/
     },
   };
